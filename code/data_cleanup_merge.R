0b7926b4969d77053f7d016e887c853e10fa82e8
Noushin Nabavi <nabavinoushin@gmail.com>
2020-04-10 12:31:00 -0700

    bug fix

--- code/data_cleanup_merge.R
+++ code/data_cleanup_merge.R
@@ -1,6 +1,6 @@
 
 # declare dependencies
-if (!exists("setup_sourced")) source(here::here("scripts", "setup.R"))
+if (!exists("setup_sourced")) source(here::here("code", "setup.R"))
 
 #---------------------------------------------------------------------
 
@@ -14,12 +14,13 @@ maps <- maps_data %>%
   mutate(country = str_replace(c, "-None", ""))
 
 maps <- maps[, -c(1,2)]
-maps <- maps[,-6]
+maps <- maps[,-8]
 
 canada <- maps %>%
-  filter(str_detect(maps$country, "CA")) 
+  filter(str_detect(maps$country, "Canada")) 
  
 library(ggExtra) 
+
 g <- ggplot(data = canada, 
        aes(x = country, y = parks)) +
   geom_point(aes(col=parks, size=workplace)) + 
@@ -27,11 +28,10 @@ g <- ggplot(data = canada,
   labs(subtitle="Stay-at-Home-Watch", 
        y="parks", 
        x="Canadian Provinces", 
-       title="Park mobility scatterplot", 
-       caption = "Source: Google Maps")
+       title="Park mobility scatterplot for Canada", 
+       caption = "Source: Google Maps") +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+ggsave("scatterplot_canada.pdf")
 
 ggMarginal(g, type = "histogram", fill="transparent")
-
-
-
 
